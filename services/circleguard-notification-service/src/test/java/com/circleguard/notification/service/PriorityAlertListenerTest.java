c30301e8b98a626f7239138d0549488183d33fcf
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-05-09 10:59:11 -0500

    removing some tags to see if test passes

--- services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/PriorityAlertListenerTest.java
+++ services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/PriorityAlertListenerTest.java
@@ -19,8 +19,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@ActiveProfiles("test")
 public class PriorityAlertListenerTest {
 
     @Mock
